2e5b4c32530bcff4c233e9c2652e4bc929901bc6
Connor James <111926482+SeeJay3@users.noreply.github.com>
2025-05-16 10:21:34 -0300

    Enhance app styling with color scheme

--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -1,11 +1,12 @@
 import { StyleSheet } from "react-native";
 import { colors } from "../../styles/color";
+import { fontFamily } from "../../styles/font-family";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
-    padding: 16,
+    padding: 20,
     backgroundColor: colors.gray[100],
   },
   
@@ -13,23 +14,22 @@ export const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
-    marginBottom: 24,
+    marginBottom: 32,
     paddingBottom: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.gray[500], 
+    borderBottomWidth: 2,
+    borderBottomColor: colors.gray[200],
   },
   
   transactions: {
     flex: 1,
-    paddingBottom: 24, 
+    paddingBottom: 24,
   },
   
   title: {
     color: colors.gray[500],
-    fontWeight: 'bold',
-    fontSize: 26,
+    fontFamily: fontFamily.bold,
+    fontSize: 28,
     textAlign: 'center',
     marginTop: 24,
   },
-}
-)
+})
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,11 +1,10 @@
 import { createBottomTabNavigator, BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
-
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-
 import { Dashboard } from "../pages/Dashboard";
 import { List } from "../pages/List";
 import { Search } from "../pages/Search";
+import { colors } from "../styles/color";
 
 type AppRoutes = {
     dashboard: undefined;
@@ -17,53 +16,57 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
-export function AppRoutes () {
+export function AppRoutes() {
     return (
         <Navigator screenOptions={{
             headerShown: false,
-            tabBarActiveTintColor: 'black',
+            tabBarActiveTintColor: colors.green.base,
+            tabBarInactiveTintColor: colors.gray[400],
             tabBarShowLabel: false,
-            tabBarLabelPosition: 'beside-icon',
-            tabBarInactiveBackgroundColor: 'white',
             tabBarStyle: {
-                backgroundColor: 'lightgrey',
-                height: 50,
+                backgroundColor: colors.gray[100],
+                height: 60,
+                borderTopWidth: 1,
+                borderTopColor: colors.gray[200],
+                paddingBottom: 8,
+                paddingTop: 8,
             }
-            }}>
-            <Screen 
-            name="dashboard" 
-            component={Dashboard}
-            options={{
-                tabBarIcon: ({ size, color }) => 
-                    <FontAwesome6 
-                     name="add" 
-                     size={26} 
-                    /> 
-            }}
+        }}>
+            <Screen
+                name="dashboard"
+                component={Dashboard}
+                options={{
+                    tabBarIcon: ({ color, size }) =>
+                        <FontAwesome6
+                            name="add"
+                            size={24}
+                            color={color}
+                        />
+                }}
             />
-            <Screen 
-            name="list" 
-            component={List}
-            options={{
-                tabBarIcon: ({ size, color }) => 
-                    <FontAwesome
-                     name="list-ul" 
-                     size={26} 
-                    /> 
-                    
-            }}
+            <Screen
+                name="list"
+                component={List}
+                options={{
+                    tabBarIcon: ({ color, size }) =>
+                        <FontAwesome
+                            name="list-ul"
+                            size={24}
+                            color={color}
+                        />
+                }}
             />
-            <Screen 
-            name="search" 
-            component={Search}
-            options={{
-                tabBarIcon: ({ size, color }) => 
-                    <FontAwesome 
-                     name="search" 
-                     size={26} 
-                    /> 
-                    
-            }}
+            <Screen
+                name="search"
+                component={Search}
+                options={{
+                    tabBarIcon: ({ color, size }) =>
+                        <FontAwesome
+                            name="search"
+                            size={24}
+                            color={color}
+                        />
+                }}
             />
         </Navigator>
     )
